4a05aadc97e91dcb485bf9ab4db8985c3aedc7e6
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-06-27 21:49:12 +0300

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // auth
+{
+    path:'',loadComponent:()=>import('./core/layout/auth-layout/auth-layout.component').then(c=>c.AuthLayoutComponent)
+
+},
+// user
+{
+    path:'',loadComponent:()=>import('./core/layout/main-layout/main-layout.component').then(c=>c.MainLayoutComponent)
+}
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+      baseUrRL:`https://abhwzcmvupkoivbmgyve.supabase.co`,
+    prod:true
+};
