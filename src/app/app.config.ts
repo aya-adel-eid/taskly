419c74448bd347605836c55be32e9f83c0beb0da
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-06-29 01:01:24 +0300

    edit in cinfig.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { apiKeyInterceptor } from './core/interceptors/interceptors/api-key.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [provideRouter(routes  , withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withViewTransitions(),
+      withHashLocation()),
     provideHttpClient(withFetch(),withInterceptors([apiKeyInterceptor]))]
 };
