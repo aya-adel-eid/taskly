bd9f6152f37630c2926071d43cb34c4610ad1dd9
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-07-17 15:23:43 +0300

    .

---
+++ src/app/features/projects/pages/epic-details-popup/epic-details-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-epic-details-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './epic-details-popup.component.html',
+  styleUrl: './epic-details-popup.component.css'
+})
+export class EpicDetailsPopupComponent {
+
+}
