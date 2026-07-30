3db143dc4f388b4c6dab59fee009569ef051b5bb
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-07-31 01:08:26 +0300

    feat: add invite member modal header UI for desktop and mobile

---
+++ src/app/features/projects/pages/invite-member-form/invite-member-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-member-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './invite-member-form.component.html',
+  styleUrl: './invite-member-form.component.css'
+})
+export class InviteMemberFormComponent {
+
+}
