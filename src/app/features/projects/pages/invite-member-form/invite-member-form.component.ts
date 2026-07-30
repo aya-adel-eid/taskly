d781c95c9af1076b047a7c83aa495676b910284c
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-07-31 01:37:47 +0300

    feat: complete invite member modal UI

--- src/app/features/projects/pages/invite-member-form/invite-member-form.component.ts
+++ src/app/features/projects/pages/invite-member-form/invite-member-form.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProjectsService } from '../../services/projects.service';
 
 @Component({
   selector: 'app-invite-member-form',
   standalone: true,
   imports: [],
   templateUrl: './invite-member-form.component.html',
-  styleUrl: './invite-member-form.component.css'
+  styleUrl: './invite-member-form.component.css',
 })
 export class InviteMemberFormComponent {
-
+  private readonly projectService = inject(ProjectsService);
+  close() {
+    this.projectService.showModelInviteMember.set(false);
+  }
 }
