3b2ce2e6507b58e2ec7b96d5fc2044bf424deac2
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-07-31 15:27:39 +0300

    feat(ui): add success toast for invitation acceptance

--- src/app/features/projects/pages/accept-invitation/accept-invitation.component.ts
+++ src/app/features/projects/pages/accept-invitation/accept-invitation.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ProjectsService } from '../../services/projects.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
+import { interval, take } from 'rxjs';
+import { ToastMassageComponent } from '../../components/toast-massage/toast-massage.component';
 
 @Component({
   selector: 'app-accept-invitation',
   standalone: true,
-  imports: [],
+  imports: [ToastMassageComponent],
   templateUrl: './accept-invitation.component.html',
   styleUrl: './accept-invitation.component.css',
 })
@@ -14,12 +16,14 @@ export class AcceptInvitationComponent implements OnInit {
   private readonly projectService = inject(ProjectsService);
   private readonly route = inject(ActivatedRoute);
   private readonly router = inject(Router);
+  successMessage = signal<string>('');
   token = signal<string>('');
   ngOnInit(): void {
     console.log(this.route.snapshot.queryParamMap.get('token'));
     this.token.set(this.route.snapshot.queryParamMap.get('token')!);
   }
   acceptInvitation() {
+    this.successMessage.set('');
     this.projectService
       .AcceptInvitation({
         p_token: this.token(),
@@ -27,7 +31,13 @@ export class AcceptInvitationComponent implements OnInit {
       .subscribe({
         next: (resp) => {
           console.log(resp);
-          this.router.navigateByUrl('/project');
+          this.successMessage.set('Invitation accepted successfully!');
+          interval(1000)
+            .pipe(take(5))
+            .subscribe(() => {
+              this.router.navigateByUrl('/project');
+              this.successMessage.set('');
+            });
         },
         error: (error: HttpErrorResponse) => {
           console.log(error);
