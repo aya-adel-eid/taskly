6a9fdbce08652dbc47937928a68333ac7a0ddabc
Aya ِAdel <ayaadelyahoo66@gmail.com>
2026-07-31 03:59:32 +0300

    feat: implement accept invitation UI

---
+++ src/app/features/projects/pages/accept-invitation/accept-invitation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accept-invitation',
+  standalone: true,
+  imports: [],
+  templateUrl: './accept-invitation.component.html',
+  styleUrl: './accept-invitation.component.css'
+})
+export class AcceptInvitationComponent {
+
+}
